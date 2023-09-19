042d9e94a7e7077588ee0c8b210bf5985312b7fc
aarjav-7 <88544107+aarjav-7@users.noreply.github.com>
2023-09-20 04:04:52 +0530

    Add files via upload
    First Commit

---
+++ vitrip/trip/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+# Create your models here.
+
+class Student:
+    id: int
+    name: str
+    regno: str
+    profilepic: str
+    hostel: str
+    email: str
+    phone: int
+
